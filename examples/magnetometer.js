813d548303d2d6abe285f5c2342b91176138f190
Alan Yorinks <MisterYsLab@gmail.com>
2015-01-04 10:21:50 -0500

    Updated to latest version of file

--- examples/magnetometer.js
+++ examples/magnetometer.js
@@ -4,71 +4,70 @@ var five = require("johnny-five"),
 
 
 var board = new five.Board({
-    io: new BeagleBone()
+  io: new BeagleBone()
 });
 
 board.on("ready", function() {
 
+  // Create a new `Magnetometer` hardware instance.
+  //
+  // five.Magnetometer();
+  //
+  // (Alias of:
+  //   new five.Compass({
+  //    device: "HMC5883L",
+  //    freq: 50,
+  //    gauss: 1.3
+  //   });
+  // )
+  //
 
-    // Create a new `Magnetometer` hardware instance.
-    //
-    // five.Magnetometer();
-    //
-    // (Alias of:
-    //   new five.Compass({
-    //    device: "HMC5883L",
-    //    freq: 50,
-    //    gauss: 1.3
-    //   });
-    // )
-    //
-
-    mag = new five.Magnetometer();
-
-
-// Properties
-
-// mag.raw
-//
-// x, y, z
-//
-
-// mag.scaled
-//
-// axis x, y, z
-//
-// based on value stored at (mag.scale)
-//
-
-// mag.heading
-//
-// Calculated heading in degrees (calibrated for magnetic north)
-//
-
-// mag.bearing
-//
-// Bearing data object
-//
+  mag = new five.Magnetometer();
 
 
-    // Magnetometer Event API
+  // Properties
 
-    // "headingchange"
-    //
-    // Fires when the calculated heading has changed
-    //
-    mag.on("headingchange", function() {
+  // mag.raw
+  //
+  // x, y, z
+  //
 
-        console.log("heading", Math.floor(this.heading));
-        console.log("bearing", this.bearing);
+  // mag.scaled
+  //
+  // axis x, y, z
+  //
+  // based on value stored at (mag.scale)
+  //
 
-    });
+  // mag.heading
+  //
+  // Calculated heading in degrees (calibrated for magnetic north)
+  //
 
-    // "read"
-    //
-    // Fires continuously, every 66ms.
-    //
-    mag.on("read", function(err, timestamp) {
-        // console.log( "read", this.axis );
-    });
+  // mag.bearing
+  //
+  // Bearing data object
+  //
+
+
+  // Magnetometer Event API
+
+  // "headingchange"
+  //
+  // Fires when the calculated heading has changed
+  //
+  mag.on("headingchange", function() {
+
+    console.log("heading", Math.floor(this.heading));
+    console.log("bearing", this.bearing);
+
+  });
+
+  // "read"
+  //
+  // Fires continuously, every 66ms.
+  //
+  mag.on("read", function(err, timestamp) {
+    // console.log( "read", this.axis );
+  });
 });
